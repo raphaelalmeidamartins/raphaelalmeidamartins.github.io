ef68fb833502bbf1b1507338e241bef4b349512c
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-05-27 17:07:44 -0300

    :construction: chore: add CssBaseline

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
+import CssBaseline from '@mui/material/CssBaseline';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HashRouter>
+      <CssBaseline />
       <App />
     </HashRouter>
   </React.StrictMode>
