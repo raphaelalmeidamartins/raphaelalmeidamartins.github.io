386abdaa960a9cf23e646de1c6ffca318f40b4ad
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-05-27 16:32:07 -0300

    :construction: feat: create the pages files and define the routes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
