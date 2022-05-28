6af73a534d3aca775224edbf435cf10800496cb5
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-05-27 22:17:14 -0300

    :sparkles: feat: add heading to the Projects page

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,3 +1,4 @@
+import { Typography } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/system/Container';
 import React from 'react';
@@ -6,21 +7,22 @@ import projectsData from '../../data/projectsData';
 
 function Projects() {
   return (
-    <Container>
+    <Container
+      sx={{
+        paddingBottom: '52px',
+        paddingTop: '52px',
+      }}
+    >
+      <Typography variant="h3" element="h1" align="center" gutterBottom>
+        Meus projetos
+      </Typography>
       <Grid
         container
         spacing={{ xs: 3, sm: 3, md: 3 }}
         columns={{ xs: 4, sm: 8, md: 12 }}
-        sx={{ maxWidth: '1200px' }}
       >
         {projectsData.map((project) => (
-          <Grid
-            item
-            xs={12}
-            sm={4}
-            md={4}
-            key={project.id}
-          >
+          <Grid item xs={12} sm={4} md={4} key={project.id}>
             <ProjectCard key={project.id} {...project} />
           </Grid>
         ))}
