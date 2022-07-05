f41d5a347a3e9f0e637b275d2d5dbeb47b98a6e0
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-05 17:00:43 -0300

    :construction: feat: add placeholder text

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -5,7 +5,27 @@ function About() {
   return (
     <>
       <Typography variant="h3" element="h1" align="center" gutterBottom>
-        Sobre
+        Sobre mim
+      </Typography>
+      <Typography variant="body1" element="p" align="left" paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In faucibus,
+        enim eu aliquet dapibus, orci lacus vehicula nisi, a tempus neque lorem
+        in justo. Mauris ac velit fringilla nibh auctor efficitur in ac sapien.
+        Quisque dictum quis ligula quis dictum. Vivamus ipsum elit, volutpat
+        eget diam a, ornare gravida nisi. Etiam sed urna sagittis, ullamcorper
+        elit eu, egestas libero. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Suspendisse pharetra viverra elit, ac maximus felis
+        venenatis sed.
+      </Typography>
+      <Typography variant="body1" element="p" align="left" paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In faucibus,
+        enim eu aliquet dapibus, orci lacus vehicula nisi, a tempus neque lorem
+        in justo. Mauris ac velit fringilla nibh auctor efficitur in ac sapien.
+        Quisque dictum quis ligula quis dictum. Vivamus ipsum elit, volutpat
+        eget diam a, ornare gravida nisi. Etiam sed urna sagittis, ullamcorper
+        elit eu, egestas libero. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Suspendisse pharetra viverra elit, ac maximus felis
+        venenatis sed.
       </Typography>
     </>
   );
