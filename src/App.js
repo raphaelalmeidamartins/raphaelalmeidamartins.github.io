2ba87f2dce6d53498ad7d56a54cc1217e58ce603
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-05 16:43:51 -0300

    :sparkles: feat: add the wrapper and footer components

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
+import Footer from './components/Footer';
+import Wrapper from './components/Wrapper';
 
 function App() {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={ <Home /> } />
-        <Route path="/about" element={ <About /> } />
-        <Route path="/projects" element={ <Projects /> } />
-        <Route path="/contact" element={ <Contact /> } />
-      </Routes>
+      <Wrapper>
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+          <Route path="/about" element={ <About /> } />
+          <Route path="/projects" element={ <Projects /> } />
+          <Route path="/contact" element={ <Contact /> } />
+        </Routes>
+      </Wrapper>
+      <Footer />
     </>
   );
 }
