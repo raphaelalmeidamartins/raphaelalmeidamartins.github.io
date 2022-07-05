f636b4c5ca74c5e293cf741dfec786643561fe21
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-05 15:26:48 -0300

    :construction: feat: add container mui component and title

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/system/Container';
 
 function Home() {
   return (
-    <h1>Home</h1>
+    <Container
+      component="main"
+      sx={{
+        paddingBottom: '52px',
+        paddingTop: '104px',
+      }}
+    >
+      <Typography variant="h3" element="h1" align="center" gutterBottom>
+          Home
+      </Typography>
+    </Container>
   );
 }
 
