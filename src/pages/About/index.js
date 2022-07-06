11772a03e20a92e545cc5e05801dd136937a0ea0
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-06 15:17:05 -0300

    :sparkles: feat: replace the placeholder text and add the Wrapper component

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,33 +1,36 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
+import Wrapper from '../../components/Wrapper';
 
 function About() {
   return (
-    <>
-      <Typography variant="h3" element="h1" align="center" gutterBottom>
-        Sobre mim
+    <Wrapper sectionId="about">
+      <Typography variant="h3" element="h1" align="left" gutterBottom>
+          Olá,
       </Typography>
       <Typography variant="body1" element="p" align="left" paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In faucibus,
-        enim eu aliquet dapibus, orci lacus vehicula nisi, a tempus neque lorem
-        in justo. Mauris ac velit fringilla nibh auctor efficitur in ac sapien.
-        Quisque dictum quis ligula quis dictum. Vivamus ipsum elit, volutpat
-        eget diam a, ornare gravida nisi. Etiam sed urna sagittis, ullamcorper
-        elit eu, egestas libero. Lorem ipsum dolor sit amet, consectetur
-        adipiscing elit. Suspendisse pharetra viverra elit, ac maximus felis
-        venenatis sed.
+          sou Raphael Martins desenvolvedor Web Jr. em formação. Escolhi a área de
+          programação porque acredito que a tecnologia é uma ferramenta capaz de
+          mudar o mundo para melhor levando informação e oportunidades.{' '}
+        <strong>Eu quero fazer parte desta mudança.</strong>
       </Typography>
       <Typography variant="body1" element="p" align="left" paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In faucibus,
-        enim eu aliquet dapibus, orci lacus vehicula nisi, a tempus neque lorem
-        in justo. Mauris ac velit fringilla nibh auctor efficitur in ac sapien.
-        Quisque dictum quis ligula quis dictum. Vivamus ipsum elit, volutpat
-        eget diam a, ornare gravida nisi. Etiam sed urna sagittis, ullamcorper
-        elit eu, egestas libero. Lorem ipsum dolor sit amet, consectetur
-        adipiscing elit. Suspendisse pharetra viverra elit, ac maximus felis
-        venenatis sed.
+          Antes de descobrir minha aptidão por desenvolvimento de software, fiz um
+          técnico em Design Gráfico e me formei em Gestão Financeira. Apesar de
+          não ter seguido carreira em nenhuma dessas áreas, essa bagagem me
+          proporciona <strong>alguns diferenciais como programador</strong>. O
+          contato com design me trouxe uma sensibilidade estética mais apurada que{' '}
+        <strong>facilita meu trabalho com CSS</strong> e construção de
+          interfaces. Enquanto meu contato com matemática financeira e estatística
+          ajuda
+        <strong> na lógica dos meus algorítimos</strong>.
       </Typography>
-    </>
+      <Typography variant="body1" element="p" align="left" paragraph>
+          Sou uma pessoa bastante criativa, com iniciativa e perfil de liderança.
+          Entre os valores que mais prezo estão o{' '}
+        <strong>comprometimento e a colaboração.</strong>
+      </Typography>
+    </Wrapper>
   );
 }
 
