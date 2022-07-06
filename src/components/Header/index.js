ece22252c115d0a33b4d0a1a30e59fb4559f94af
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-06 15:20:08 -0300

    :recycle: refactor: change the button components to be anchours
    Each button redirects to the specific section

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,50 @@
+import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Container from '@mui/system/Container';
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
 function Header() {
-  const navigate = useNavigate();
-
   return (
-    <AppBar sx={{ padding: '4px 0' }}>
-      <Container sx={{ display: 'flex', justifyContent: 'center' }}>
-        <ButtonGroup variant="text">
-          <Button onClick={ () => navigate('/') } size="large" sx={{ color: 'white' }}>Home</Button>
-          <Button onClick={ () => navigate('/about') } size="large" sx={{ color: 'white' }}>Sobre</Button>
-          <Button onClick={ () => navigate('/projects') } size="large" sx={{ color: 'white' }}>Projetos</Button>
-          <Button onClick={ () => navigate('/contact') } size="large" sx={{ color: 'white' }}>Contato</Button>
+    <AppBar
+      sx={{
+        padding: '4px 0',
+      }}
+    >
+      <Container sx={{ display: 'flex', justifyContent: 'right' }}>
+        <ButtonGroup component="nav" variant="text">
+          <Button
+            component="a"
+            href="#home"
+            size="large"
+            sx={{ color: 'white' }}
+          >
+            Home
+          </Button>
+          <Button
+            component="a"
+            href="#about"
+            size="large"
+            sx={{ color: 'white' }}
+          >
+            Sobre
+          </Button>
+          <Button
+            component="a"
+            href="#projects"
+            size="large"
+            sx={{ color: 'white' }}
+          >
+            Projetos
+          </Button>
+          <Button
+            component="a"
+            href="#contact"
+            size="large"
+            sx={{ color: 'white' }}
+          >
+            Contato
+          </Button>
         </ButtonGroup>
       </Container>
     </AppBar>
