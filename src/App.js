d767aee7064f9468e8d32fe019056f5f02b91d96
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-06 15:18:10 -0300

    :recycle: refactor: remove the routes
    Insted of routes, now every page is now a section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
-import Footer from './components/Footer';
-import Wrapper from './components/Wrapper';
 
 function App() {
   return (
     <>
       <Header />
-      <Wrapper>
-        <Routes>
-          <Route path="/" element={ <Home /> } />
-          <Route path="/about" element={ <About /> } />
-          <Route path="/projects" element={ <Projects /> } />
-          <Route path="/contact" element={ <Contact /> } />
-        </Routes>
-      </Wrapper>
+      <main>
+        <Home />
+        <About />
+        <Projects />
+        <Contact />
+      </main>
       <Footer />
     </>
   );
