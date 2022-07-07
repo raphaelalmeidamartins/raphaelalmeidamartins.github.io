127814a733361c9983718e1ef3ebca68c77acc74
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-07 01:34:11 -0300

    :lipstick: style: add svg background

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
+import background from './assets/other-images/scattered-forcefields.svg';
 
 function App() {
   return (
     <>
       <Header />
-      <main>
+      <main style={{
+        backgroundColor: '#3B0225',
+        backgroundImage: `url(${background})`,
+        backgroundAttachment: 'fixed',
+        backgroundSize: 'cover',
+      }}>
         <Home />
         <About />
         <Projects />
