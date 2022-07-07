0f6cbac7429f969cf6ef850fd1b391438e83c033
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-07 01:27:47 -0300

    :sparkles: feat: add color palette to the theme and change fontFamily

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,9 +1,31 @@
 import { createTheme } from '@mui/material';
 
 const theme = createTheme({
-  mode: 'dark',
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#D67BA8',
+    },
+    secondary: {
+      main: '#d86983',
+      contrastText: '#000000',
+    },
+    error: {
+      main: '#ef7a52',
+    },
+    text: {
+      primary: '#eac9ef',
+      secondary: 'rgba(234,201,239,0.7)',
+      disabled: 'rgba(234,201,239,0.5)',
+      hint: 'rgba(234,201,239,0.5)',
+    },
+    background: {
+      default: '#3b0225',
+      paper: '#540533',
+    },
+  },
   typography: {
-    fontFamily: '"Kantumruy Pro", "Helvetica", "Arial", sans-serif',
+    fontFamily: '"Nunito Sans", "Helvetica", "Arial", sans-serif',
   },
 });
 
