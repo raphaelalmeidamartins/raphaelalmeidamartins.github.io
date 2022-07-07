1139ad0718ef6c3263612c01a0d25a97b514725e
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-07 01:29:55 -0300

    :sparkles: feat(Wrapper): add minHeight prop

--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Container from '@mui/system/Container';
 
-function Wrapper({ children, sectionId }) {
+function Wrapper({ children, sectionId, minHeight }) {
   return (
     <Container
       id={ sectionId }
       component="section"
       sx={{
-        minHeight: '100vh',
-        paddingBottom: '104px',
-        paddingTop: '104px',
+        minHeight,
+        paddingBottom: '52px',
+        paddingTop: '52px',
       }}
     >
       {children}
@@ -24,6 +24,7 @@ Wrapper.propTypes = {
     PropTypes.arrayOf(PropTypes.node),
   ]),
   sectionId: PropTypes.string,
+  minHeight: PropTypes.string,
 };
 
 export default Wrapper;
