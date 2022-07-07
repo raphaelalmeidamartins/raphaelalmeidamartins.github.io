62402c2f2c57cd5cb209f3b67ec5de72ff3eb59c
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-07 20:37:19 -0300

    :sparkles: feat: Header is now responsive and beautiful

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,64 +1,117 @@
-import React from 'react';
-import { Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import Container from '@mui/system/Container';
+import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Toolbar from '@mui/material/Toolbar';
+import * as React from 'react';
+import { FaGithubAlt, FaLinkedinIn } from 'react-icons/fa';
 
-function Header() {
+const sections = [
+  { name: 'Inicio', href: '#home' },
+  { name: 'Sobre', href: '#about' },
+  { name: 'Projetos', href: '#projects' },
+  { name: 'Contato', href: '#contact' },
+];
+
+const Header = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
-    <AppBar
-      sx={{
-        padding: '4px 0',
-      }}
-    >
-      <Container
-        sx={{
-          alignItems: 'center',
-          display: 'flex',
-          justifyContent: 'space-between',
-        }}
-      >
-        <Typography variant="h5" element="h1" align="left">
-          {'<RaphaelMartins />'}
-        </Typography>
-        <ButtonGroup component="nav" variant="text">
-          <Button
-            component="a"
-            href="#home"
-            size="large"
-            sx={{ color: 'white' }}
-          >
-            Home
-          </Button>
-          <Button
-            component="a"
-            href="#about"
-            size="large"
-            sx={{ color: 'white' }}
-          >
-            Sobre
-          </Button>
-          <Button
-            component="a"
-            href="#projects"
-            size="large"
-            sx={{ color: 'white' }}
-          >
-            Projetos
-          </Button>
-          <Button
-            component="a"
-            href="#contact"
-            size="large"
-            sx={{ color: 'white' }}
-          >
-            Contato
-          </Button>
-        </ButtonGroup>
+    <AppBar>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {sections.map(({ name, href }) => (
+                <MenuItem key={name} onClick={handleCloseNavMenu}>
+                  <Button
+                    variant="text"
+                    component="a"
+                    href={href}
+                    textAlign="center"
+                  >
+                    {name}
+                  </Button>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {sections.map(({ name, href }) => (
+              <Button
+                key={name}
+                component="a"
+                onClick={handleCloseNavMenu}
+                href={href}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {name}
+              </Button>
+            ))}
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            <IconButton
+              size="large"
+              component="a"
+              href="https://github.com/raphaelalmeidamartins"
+              target="_blank"
+              sx={{ margin: '0 6px', p: 0 }}
+            >
+              <FaGithubAlt />
+            </IconButton>
+            <IconButton
+              size="large"
+              component="a"
+              href="https://www.linkedin.com/in/raphaelameidamartins/"
+              target="_blank"
+              sx={{ margin: '0 6px', p: 0 }}
+            >
+              <FaLinkedinIn />
+            </IconButton>
+          </Box>
+        </Toolbar>
       </Container>
     </AppBar>
   );
-}
+};
 
 export default Header;
