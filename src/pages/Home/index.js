f08648ca54fb68e491d1035b3e2e38bdd517067c
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-07 15:45:21 -0300

    :construction: feat: create Home headings

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/system/Box';
 import Wrapper from '../../components/Wrapper';
 
 function Home() {
   return (
     <Wrapper sectionId="home" minHeight="100vh">
-      {/* under development */}
+      <Box
+        sx={{
+          alignItems: 'flex-start',
+          display: 'flex',
+          flexFlow: 'column nowrap',
+          justifyContent: 'center',
+          minHeight: '90vh',
+          width: '100%',
+        }}
+      >
+        <Typography
+          variant="h1"
+          element="h1"
+          align="left"
+          sx={{ fontFamily: 'Righteous' }}
+        >
+          Raphael Martins
+        </Typography>
+        <Typography
+          variant="h3"
+          element="p"
+          align="left"
+          paragraph
+          sx={{ fontFamily: 'Righteous' }}
+        >
+          Web Developer
+        </Typography>
+      </Box>
     </Wrapper>
   );
 }
