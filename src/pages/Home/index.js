e3ba61f3be86abd5982ad1fa9bd40dbcf8fc13f4
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-07-07 01:28:54 -0300

    :construction: feat: remove placeholder text

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import Typography from '@mui/material/Typography';
 import Wrapper from '../../components/Wrapper';
 
 function Home() {
   return (
-    <Wrapper sectionId="home">
-      <Typography variant="h3" element="h1" align="center" gutterBottom>
-        Home
-      </Typography>
+    <Wrapper sectionId="home" minHeight="100vh">
+      {/* under development */}
     </Wrapper>
   );
 }
